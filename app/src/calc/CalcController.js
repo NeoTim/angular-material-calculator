c9329c89cc18dfaf95b819891046ec90d2e2c2f2
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2016-03-24 00:38:06 +0100

    init calc.display

--- app/src/calc/CalcController.js
+++ app/src/calc/CalcController.js
@@ -5,11 +5,13 @@
        .controller('CalcController', function() {
          this.result = 0;
          this.input = 0;
+         this.display = 0;
          this.numbers = [",","0","c","1","2","3","4","5","6","7","8","9"];
          this.numbersRow = [4,3,2,1,0];
          this.operators = ["÷","×","−","+"];
          this.inputNumber = function(x) {
-           this.input = this.result+x
+           this.input = this.input+x
+           this.display = this.display+x
            this.result = this.input
          }
        });
