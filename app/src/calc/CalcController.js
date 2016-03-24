5d9d9cdfe2012989ec312dafd6782a4c8380f70d
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2016-03-24 15:52:03 +0100

    clear

--- app/src/calc/CalcController.js
+++ app/src/calc/CalcController.js
@@ -5,23 +5,28 @@
        .controller('CalcController', function() {
          this.result = 0;
          this.input = 0;
-         this.lastNumber = true;
-         this.numbers = [",","0","c","1","2","3","4","5","6","7","8","9"];
+         this.numbers = [",","0","←","1","2","3","4","5","6","7","8","9"];
          this.numbersRow = [4,3,2,1,0];
-         this.operators = ["÷","×","−","+"];
+         this.operators = ["÷","×","-","+"];
          this.inputNumber = function(x) {
-           if (this.input === 0) {
-              this.input = x;
+           if (this.input === 0 && x != "←") {
+             this.input = x;
+           } else if (x == "←") {
+             if (this.input.length > 1) {
+               this.input = this.input.substring(0,this.input.length-1);
+             } else {
+               this.input = 0;
+               this.result = 0;
+             }
            } else {
              this.input = this.input+x;
            }
            console.log(this.input);
            console.log(this.input.replace(/÷/g,"/").replace(/×/g,"*"));
            this.result = eval(this.input.replace(/÷/g,"/").replace(/×/g,"*"));
-           this.lastNumber = true;
          };
          this.inputOperator = function(x) {
-           if (this.lastNumber) {
+           if (["0","1","2","3","4","5","6","7","8","9"].includes(this.input.substring(this.input.length-1,this.input.length))) {
              this.input = this.input+x;
            } else {
              this.input = this.input.substring(0,this.input.length-1)+x;
