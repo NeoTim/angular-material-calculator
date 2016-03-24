768b93f28b8fc6a0b7f1d5234dd82e5e622c8add
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2016-03-24 01:43:55 +0100

    basic math

--- app/src/calc/CalcController.js
+++ app/src/calc/CalcController.js
@@ -5,15 +5,29 @@
        .controller('CalcController', function() {
          this.result = 0;
          this.input = 0;
-         this.display = 0;
+         this.lastNumber = true;
          this.numbers = [",","0","c","1","2","3","4","5","6","7","8","9"];
          this.numbersRow = [4,3,2,1,0];
          this.operators = ["÷","×","−","+"];
          this.inputNumber = function(x) {
-           this.input = this.input+x
-           this.display = this.display+x
-           this.result = this.input
-         }
+           if (this.input === 0) {
+              this.input = x;
+           } else {
+             this.input = this.input+x;
+           }
+           console.log(this.input);
+           console.log(this.input.replace(/÷/g,"/").replace(/×/g,"*"));
+           this.result = eval(this.input.replace(/÷/g,"/").replace(/×/g,"*"));
+           this.lastNumber = true;
+         };
+         this.inputOperator = function(x) {
+           if (this.lastNumber) {
+             this.input = this.input+x;
+           } else {
+             this.input = this.input.substring(0,this.input.length-1)+x;
+           }
+           this.lastNumber = false;
+         };
        });
 
 })();
